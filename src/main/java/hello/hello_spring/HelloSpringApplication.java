3d37030a390d03a33b336394bddc9a946252fd22
Clt689 <kx1302@naver.com>
2025-02-04 17:36:13 +0900

    test commit

--- src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloSpringApplication {
-	d
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
